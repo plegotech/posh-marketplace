04051260450e830b19d6f5a75cc4a81a5942ade2
Muhammad Habib <habib.m@HT.Local>
2022-01-11 00:48:13 +0500

    created seller registration page

---
+++ resources/views/habib/sellerregistration2.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="" class="">
+    <div class="seller-registration">
+        <div class="container-fluid">
+        <div class="row">
+                <div class="col-sm-12"><h1 class="main-title text-center my-2">Create Account</h1></div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 sellerReg-opt active"><span class="number-opt-b">1</span> Personal Details</div>
+                <div class="col-sm-6 sellerReg-opt active"><span class="number-opt-b">1</span> Personal Details</div>
+            </div>
+            <div class="container my-5">
+            <div class="tier-box-area">    
+                <div class="row">
+                    <div class="col-12 text-center"><h1 class="main-title mb-4">Select Tier</div></div>
+                    </div>
+                    <div class="row">
+                    <div class="col-sm-4 text-center">
+                        <div class="box-a-tier">
+                            <h4 class="txt-blue">Free Tier</h4>
+                            <p>Only 10 Products</p>
+                            <button class="primary">START NOW</button>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 text-center">
+                        <div class="box-a-tier">
+                            <h4 class="txt-blue">Tier 2</h4>
+                            <p>Only 10 Products</p>
+                            <button class="primary">START NOW</button>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 text-center">
+                        <div class="box-a-tier">
+                            <h4 class="txt-blue">Tier 2</h4>
+                            <p>Only 10 Products</p>
+                            <button class="primary">START NOW</button>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+@endsection
