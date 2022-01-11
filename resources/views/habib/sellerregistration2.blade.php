45cd36da015330a23aea6819db1eb98cdb5c246c
Muhammad Habib <habib.m@HT.Local>
2022-01-11 20:22:04 +0500

    seller registration 2 styles

--- resources/views/habib/sellerregistration2.blade.php
+++ resources/views/habib/sellerregistration2.blade.php
@@ -14,35 +14,12 @@
             <div class="container my-5">
             <div class="tier-box-area">    
                 <div class="row">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
                     <div class="col-12 text-center"><h1 class="main-title mb-4">Select Tier</h1></div>
-=======
-                    <div class="col-12 text-center"><h1 class="main-title mb-4">Select Tier</div></div>
->>>>>>> 0405126 (created seller registration page)
-=======
-                    <div class="col-12 text-center"><h1 class="main-title mb-4">Select Tier</h1></div>
->>>>>>> c063605 (updates)
-=======
-                    <div class="col-12 text-center"><h1 class="main-title mb-4">Select Tier</h1></div>
->>>>>>> c063605c6ea2c71491537815b799558af3de46be
                     </div>
                     <div class="row">
                     <div class="col-sm-4 text-center">
                         <div class="box-a-tier">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
                             <i class="num-count-bx">1</i>
-=======
->>>>>>> 0405126 (created seller registration page)
-=======
-                            <i class="num-count-bx">1</i>
->>>>>>> c063605 (updates)
-=======
-                            <i class="num-count-bx">1</i>
->>>>>>> c063605c6ea2c71491537815b799558af3de46be
                             <h4 class="txt-blue">Free Tier</h4>
                             <p>Only 10 Products</p>
                             <button class="primary">START NOW</button>
@@ -50,18 +27,7 @@
                     </div>
                     <div class="col-sm-4 text-center">
                         <div class="box-a-tier">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
                         <i class="num-count-bx">2</i>
-=======
->>>>>>> 0405126 (created seller registration page)
-=======
-                        <i class="num-count-bx">2</i>
->>>>>>> c063605 (updates)
-=======
-                        <i class="num-count-bx">2</i>
->>>>>>> c063605c6ea2c71491537815b799558af3de46be
                             <h4 class="txt-blue">Tier 2</h4>
                             <p>Only 10 Products</p>
                             <button class="primary">START NOW</button>
@@ -69,63 +35,21 @@
                     </div>
                     <div class="col-sm-4 text-center">
                         <div class="box-a-tier">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
                         <i class="num-count-bx">3</i>
-=======
->>>>>>> 0405126 (created seller registration page)
-=======
-                        <i class="num-count-bx">3</i>
->>>>>>> c063605 (updates)
-=======
-                        <i class="num-count-bx">3</i>
->>>>>>> c063605c6ea2c71491537815b799558af3de46be
                             <h4 class="txt-blue">Tier 2</h4>
                             <p>Only 10 Products</p>
                             <button class="primary">START NOW</button>
                         </div>
                     </div>
                 </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c063605 (updates)
-=======
->>>>>>> c063605c6ea2c71491537815b799558af3de46be
                 
                 <div class="row mt-5">
                 <div class="col-sm-12 text-center">
                     <h1 class="main-title mb-4">Select Product Categories</h1>
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    <div class="input-group justify-content-center select-pro-cat">
-                    <select v-model="selected">
-                        <option value="all">All</option>
-                        <option>A</option>
-                        <option>B</option>
-                        <option>C</option>
-                    </select>
-                <input type="text" class="form-control" placeholder="Browse Category">
-                <div class="input-group-append">
-                    <button class="primary" type="button"> <i class="fa fa-search"></i></button> </div>
-            </div>
-                </div>                
-                </div>
-
-                <div class="row mt-5">
-=======
-=======
->>>>>>> c063605c6ea2c71491537815b799558af3de46be
                 </div>                
                 </div>
 
                 <div class="row">
-<<<<<<< HEAD
->>>>>>> c063605 (updates)
-=======
->>>>>>> c063605c6ea2c71491537815b799558af3de46be
                 <div class="col-sm-12 text-center">
                     <h1 class="main-title mb-4">Select Template</h1>
                 </div>                
@@ -139,45 +63,15 @@
                     <img src="/img/templae-sample-3.png">
                 </div>
                 </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
 
                 <div class="row mt-5 ">
                     <div class="col-sm-12 text-center"><h2 class="main-title">Grab Your .com</h2></div>
                     <div class="col-sm-12">
-                    <div class="input-group justify-content-center">
+                    <div class="input-group justify-content-center select-temp">
                 <input type="text" class="form-control" placeholder="williamson.com">
                 <div class="input-group-append">
                     <button class="primary" type="button">
                         Search <i class="fa fa-search"></i>
-=======
-=======
->>>>>>> c063605c6ea2c71491537815b799558af3de46be
-<style>
-    .has-search .form-control-feedback {
-    position: absolute;
-    z-index: 2;
-    display: block;
-    width: 2.375rem;
-    height: 2.375rem;
-    line-height: 2.375rem;
-    text-align: center;
-    pointer-events: none;
-    color: #aaa;
-}
-    </style>
-                <div class="row  ">
-                    <div class="col-sm-12"><h2 class="main-title">Grab Your .com</h2></div>
-                    <div class="col-sm-12">
-                    <div class="input-group justify-content-center">
-                <input type="text" class="form-control" placeholder="Search this blog">
-                <div class="input-group-append">
-                    <button class="primary" type="button">
-                        <i class="fa fa-search"></i>
-<<<<<<< HEAD
->>>>>>> c063605 (updates)
-=======
->>>>>>> c063605c6ea2c71491537815b799558af3de46be
                     </button>
                 </div>
             </div>
@@ -185,14 +79,6 @@
                 </div>
                 </div>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 0405126 (created seller registration page)
-=======
->>>>>>> c063605 (updates)
-=======
->>>>>>> c063605c6ea2c71491537815b799558af3de46be
                 </div>
             </div>
         </div>
