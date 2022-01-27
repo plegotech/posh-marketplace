0258a6568646f2f2d83728591b7e3484becafdc3
osd_dev <osd.dev@express.com.pk>
2022-01-27 06:02:11 +0500

    updates for

--- resources/js/components/vendor/CompletedOrders.vue
+++ resources/js/components/vendor/CompletedOrders.vue
@@ -48,6 +48,24 @@
                             </div>
                             </div>
                                 </div>
+                                <div class="col">
+                                    <select class="mt-0 select-custom-point">
+                                        <option value="" selected>Choose Month</option>
+                                        <option value="Janruary">Janruary</option>
+                                        <option value="February">February</option>
+                                        <option value="March">March</option>
+                                        <option value="April">April</option>
+                                        <option value="May">May</option>
+                                        <option value="June">June</option>
+                                        <option value="July">July</option>
+                                        <option value="August">August</option>
+                                        <option value="September">September</option>
+                                        <option value="October">October</option>
+                                        <option value="November">November</option>
+                                        <option value="December">December</option>
+                                    </select>
+                                </div>
+                            </div>
                             </div>
                         </div>
                         <hr>
