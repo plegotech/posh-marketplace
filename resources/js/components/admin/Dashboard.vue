ac5e548128d92744980a32ae06c6323839de2d94
Mashhood <mashood@plego.com>
2022-02-15 03:18:38 +0500

    Graph implimentation

--- resources/js/components/admin/Dashboard.vue
+++ resources/js/components/admin/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container-fluid">
+    
         <div class="row">
             <div class="col-sm-3">
                 <div class="datepicker-d">
@@ -31,6 +32,22 @@
         <br>
         <div class="row">
             <h1>Analytics</h1>
+            <div class="col-sm-6">
+                <canvas id="barChart"></canvas>
+            <br>
+            </div>
+            <div class="col-sm-6">
+                <canvas id="polarArea"></canvas>
+            <br>
+            </div>
+            <div class="col-sm-6">
+                <canvas id="doughnut"></canvas>
+            <br>
+            </div>
+            <div class="col-sm-6">
+                <canvas id="line_Chart"></canvas>
+            <br>
+            </div>
             <div class="col-sm-4">
                 <div class="box">
                     <div class="left">
@@ -441,6 +458,111 @@ export default {
                     document.getElementById('ajaxLoader').style.display = 'none';
                 });
         }
+    },
+    mounted() {
+        //Line Chart Begins from here!
+        var line_Chart = document.getElementById('line_Chart');
+        Chart.defaults.line.spanGaps = true;
+        var stackedLine = new Chart(line_Chart, {
+            type: 'line',
+            data: {
+                label: ["1", "2", "3", "4", "5", "6", "7"],
+                xAxisID: ['4', '2', '8', '7', '16', '18', '6', '20'],
+                yAxisID: ['4', '2', '8', '7', '16', '18', '6', '20'],
+                datasets: [{
+                    label: 'My First Dataset',
+                    data: [0, 20],
+                    fill: true,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                }]
+                },
+        });
+
+        //Bar Chart Begins from here!
+        var barChart = document.getElementById('barChart');
+        var barChart = new Chart(barChart, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
+
+        //Doughnut Begins Here!
+        var doughnut_ctx = document.getElementById('doughnut');
+        var doughnut = new Chart(doughnut_ctx, {
+            type: 'doughnut',
+            data: {
+                datasets: [{
+                    data: [15, 20, 15],
+                    backgroundColor: [
+                        '#0862a3',
+                        '#a30808',
+                        '#08a30d'
+                    ]
+                }],
+                labels: [
+                    'Blue',
+                    'Red',
+                    'Green'
+                ],
+            },
+        });
+
+
+        //PolarArea Chart Begins Here!
+        var polarArea_ctx = document.getElementById('polarArea');
+        var polarArea = new Chart(polarArea_ctx, {
+            type: 'polarArea',
+            data: {
+                datasets: [{
+                    data: [15, 20, 10, 5],
+                    backgroundColor: [
+                        '#d3d06e',
+                        '#6ed3d3',
+                        '#826ed3',
+                        '#d36ec7'
+                    ]
+                }],
+                labels: [
+                    'Yellow',
+                    'Sky Blue',
+                    'Purple',
+                    'Pink'
+                ],
+            },
+        });
     }
 }
 </script>
