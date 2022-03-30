ab0821075afc77cda95bb5579c8e65c71e2a796b
Teerath Kumar <teerath@plego.com>
2022-03-31 04:22:55 +0500

    Order Updates
    Order Updates

--- app/Http/Controllers/CartItemsController.php
+++ app/Http/Controllers/CartItemsController.php
@@ -12,12 +12,14 @@ class CartItemsController extends Controller {
     public function addToCart(Request $request) {
 //        dd($request);
         $data = $request->all();
+        
+        //return response()->json($data);
         $model = CartItems::insert($request->all());
         if ($model) {
             return Response()->json(array(
                         'success' => true,
                         //'message' => $data
-                        'message' => User::where('id',$data->user_id)->with('cartitems')->first()
+                        'message' => User::where('id',$data["user_id"])->with('cartitems')->first()
             )); // 400 being the HTTP code for an invalid request.
         } else {
             return Response()->json(array(
@@ -27,5 +29,25 @@ class CartItemsController extends Controller {
         }
         //return response()->json($model);
     }
+    public function removeCartData(Request $request){
+        $data = $request->all();
+        $model = CartItems::where('user_id', $data['user_id'])->delete();
+        if ($model) {
+            return Response()->json(array(
+                        'success' => true
+            )); // 400 being the HTTP code for an invalid request.
+        } else {
+            return Response()->json(array(
+                        'success' => false
+            )); // 400 being the HTTP code for an invalid request.
+        }
 
+    }
+
+    public function userCartData(Request $request){
+        $data = $request->all();
+        $response = CartItems::where('user_id', $data['user_id'])->with('productsTable')->get();
+        
+        return response()->json($response);
+    }
 }
