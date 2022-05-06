3d6d6f42517b7d3575c978c6335a9c6400a8469d
Teerath Kumar <teerath@plego.com>
2022-05-06 23:11:46 +0500

    API Updates
    API Updates

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -60,6 +60,31 @@ class CategoryController extends Controller {
         }
         //return response()->json($data);
     }
+    public function fetchWithLimit(Request $request) {
+        $dataReq = $request->all();
+        $p_cat = isset($dataReq['id']) ? $dataReq['id'] : 0;
+        $data = \App\Category::where([['parent_category_id', $p_cat], ['status', 1]])->with('children')->limit(10)->get();
+        if ($data) {
+            $myAr = array();
+            foreach ($data as $row) {
+                $new_title = strtolower(preg_replace('/\s+/', '', $row['title']));
+                $new_title = str_replace("&", "-", $new_title);
+                $row['newtitle'] = $new_title;
+                $myAr[] = $row;
+            }
+            $data = $myAr;
+            return Response()->json(array(
+                        'success' => true,
+                        'data' => $data
+            )); // 400 being the HTTP code for an invalid request.
+        } else {
+            return Response()->json(array(
+                        'success' => false,
+                        'data' => "No Data"
+            )); // 400 being the HTTP code for an invalid request.
+        }
+        //return response()->json($data);
+    }
     public function fetchAll(){
         $data = \App\Category::with('children')->get();
         if($data){
