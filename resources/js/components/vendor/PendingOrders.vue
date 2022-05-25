9266f71e2836674572546005cd9a29823a3f9018
habibplego <97044005+habibplego@users.noreply.github.com>
2022-05-25 22:25:07 +0500

    updates for the vendor View details

--- resources/js/components/vendor/PendingOrders.vue
+++ resources/js/components/vendor/PendingOrders.vue
@@ -25,38 +25,44 @@
                         <!-- START: VIEW PRODUCT -->
                         <div class="row">
                             <div class="col-12">
+                                <div class="details-bx-area">
                                 <div class="row">
                                      <div class="col-sm-8">
-                                        <div class="vendor-rod-imag"><img src="https://posh-temp1.plego.pro/assets/profile.6f7406c7.png" class="img-x-v-view"></div>
+                                         <div class="vendorBox-left">
+                                        <div class="vendor-rod-imag"><img src="https://nypost.com/wp-content/uploads/sites/2/2021/10/anti-aging-skincare-products.jpg" class="img-x-v-view"></div>
                                         <div class="vendor-detail-bx">
                                             <h5>Organic Balloon Garland</h5>
                                             <div class="ven-q-bx">Quantity Needed: 1</div>
                                             <div class="ven-q-bx">Order Date: May 09, 2022 | 10:20 PM</div>
+                                        </div>
                                         </div>
                                     </div>
                                     <div class="col-sm-4">
                                         <div class="vendor-rod-detail">
                                             <div class="topaid-bx">Total Paid:</div>
                                             <div class="topaid-amout">$355.06</div>
+                                            <div class="topaid-amout"><a href="#">Refund</a></div>
                                         </div>
                                     </div>
                                 </div>
+                                </div>
+                                <div class="details-bx-area">
                                 <div class="row">
                                     <div class="col-sm-3">
                                         <div class="box-vender-view">
-                                            <h5>User Name</h5>
+                                            <h5>Seller Name</h5>
                                             <p>Lauren Graduation Party</p>
                                         </div>
                                     </div>
                                     <div class="col-sm-3">
                                         <div class="box-vender-view">
-                                            <h5>User Email</h5>
+                                            <h5>Seller Email</h5>
                                             <p>Lauren Graduation Party</p>
                                         </div>
                                     </div>
                                     <div class="col-sm-3">
                                         <div class="box-vender-view">
-                                            <h5>User Phone Number</h5>
+                                            <h5>Seller Phone Number</h5>
                                             <p>Lauren Graduation Party</p>
                                         </div>
                                     </div>
@@ -66,6 +72,17 @@
                                             <p>Lauren Graduation Party</p>
                                         </div>
                                     </div>
+                                </div>
+                                </div>
+                                <div class="details-bx-area">
+                                <div class="row">
+                                    <div class="col-sm-9">
+                                        <div class="box-vender-view">
+                                        <h5>Description of Product</h5>
+                                        <p>Description of Product Description of Product Description of Product Description of Product Description of Product Description of Product </p>
+                                        </div>
+                                    </div>
+                                </div>
                                 </div>
                             </div>
                         </div>
